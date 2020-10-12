2db955c9cde2c0557559683d81d635b95a830263
Kristijan Sedlak <xpepermint@gmail.com>
2020-10-12 22:43:12 +0200

    Refactor encoder

--- huffman/src/lib.rs
+++ huffman/src/lib.rs
@@ -1,3 +1,11 @@
+//! This module implementing [Canonical Huffman] functionality handling [HPACK]
+//! format in [HTTP/2]. It exposes a simple API for performing the encoding and
+//! decoding of [HTTP/2] header string literals according to the [HPACK] spec.
+//! 
+//! [HPACK]: https://tools.ietf.org/html/rfc7541
+//! [HTTP/2]: https://tools.ietf.org/html/rfc7540
+//! [Canonical Huffman]: https://en.wikipedia.org/wiki/Canonical_Huffman_code
+
 mod builder;
 mod decoder;
 mod encoder;
