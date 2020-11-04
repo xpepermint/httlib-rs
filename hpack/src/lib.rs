4a3472c9e089f3045b51effc4160a48be091078e
Kristijan Sedlak <xpepermint@gmail.com>
2020-11-04 10:33:11 +0100

    Add hpack readme

--- hpack/src/lib.rs
+++ hpack/src/lib.rs
@@ -1,7 +1,44 @@
-mod decode;
-mod encode;
+//! This crate implements [HPACK], a compression format for efficiently
+//! representing HTTP header fields in [HTTP/2]. It exposes a simple API for
+//! performing the encoding and decoding of HTTP headers.
+//! 
+//! [![Documentation](https://img.shields.io/badge/-Documentation-blue?style=for-the-badge&logo=Rust)](https://docs.rs/httlib-hpack)
+//! [![Source](https://img.shields.io/badge/-Source-lightgrey?style=for-the-badge&logo=GitHub)](https://github.com/xpepermint/httlib-rs/tree/main/hpack)
+//! 
+//! ## Usage
+//!
+//! Encoding:
+//! 
+//! ```rs
+//! use httlib_hpack::Encoder;
+//! 
+//! let mut sequence = Vec::new();
+//! let mut encoder = Encoder::default();
+//! encoder.encode(b":method", b"GET")?;
+//! ```
+//! 
+//! Decoding:
+//! 
+//! ```rs
+//! use httlib_hpack::Decoder;
+//!
+//! let mut dst = Vec::new();
+//! let mut src = vec![168, 209, ...];
+//! let mut decoder = Decoder::default();
+//! decoder.decode(&mut src, &mut dst)?;
+//! ```
+//! 
+//! ## Articles
+//! 
+//! * [HPACK: The secret ingredient of HTTP/2](TODO)
+//! 
+//! [HPACK]: https://tools.ietf.org/html/rfc7541
+//! [HTTP/2]: https://tools.ietf.org/html/rfc7540
+
+mod decoder;
+mod encoder;
 mod table;
 
-pub use decode::*;
-pub use encode::*;
+pub use decoder::*;
+pub use encoder::*;
 pub use table::*;
