9d21582644d65b0ae7d084904f078747e2dcbbf8
Kristijan Sedlak <xpepermint@gmail.com>
2020-11-10 09:20:05 +0100

    Add about hpack

--- hpack/src/lib.rs
+++ hpack/src/lib.rs
@@ -5,6 +5,30 @@
 //! [![Documentation](https://img.shields.io/badge/-Documentation-blue?style=for-the-badge&logo=Rust)](https://docs.rs/httlib-hpack)
 //! [![Source](https://img.shields.io/badge/-Source-lightgrey?style=for-the-badge&logo=GitHub)](https://github.com/xpepermint/httlib-rs/tree/main/hpack)
 //! 
+//! ## About
+//! 
+//! [HPACK] introduces a completely new approach to the header packaging and
+//! management. Websites today require dozens or hundreds of requests and the
+//! redundant header fields in these requests consume bandwidth unnecessarily.
+//! Therefore, [HPACK] is a compressor, which's main function is to eliminate
+//! redundant header fields. 
+//! 
+//! The specification is rather short, but as it goes for other [HTTP/2] related
+//! specifications, this one is also often unclear and ambiguous, creating
+//! numerous issues and uncertainty for implementers. It is also written with an
+//! experienced developer in mind and focuses primarily on the decoder
+//! functioning and assumes that the implementor will be knowledgeable enough to
+//! add all details as he sees are needed for the working product. 
+//! 
+//! On top of that, a significant shift in thinking is required from the
+//! implementer of the [HTTP/2] protocol. A connection in [HTTP/2] is not a
+//! single request/response. We can start multiple simultaneous streams in one
+//! connection, representing multiple request/response sessions, which was not
+//! possible in the previous versions of the HTTP protocol. The [HPACK]
+//! compressor uses this characteristic of [HTTP/2] by indexing headers
+//! considering the whole connection and not per stream, which might seem
+//! somewhat unusual.
+//! 
 //! ## Usage
 //!
 //! **Encoding example:**
